4c7c3bb048929101d26760ec37d5fa90a0c5d3a3
Jonathan Williamson <jon@pimoroni.com>
2017-06-15 15:07:21 +0000

    Added starfield demo

---
+++ examples/stars.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import colorsys
+import math
+import time
+import random
+
+import unicornhathd
+
+
+print("""Unicorn HAT HD: Stars
+
+This example simulates a wooshing star field.
+
+Press Ctrl+C to exit!
+
+""")
+
+star_count = 25
+star_speed = 0.10
+stars = []
+
+for i in range(0, star_count):
+  stars.append((random.uniform(4, 11), random.uniform(4, 11), 0))
+
+try:
+    while True:
+      unicornhathd.clear()
+
+      for i in range(0, star_count):
+        stars[i] = (
+            stars[i][0] + ((stars[i][0] - 8.1) * star_speed),
+            stars[i][1] + ((stars[i][1] - 8.1) * star_speed),
+            stars[i][2] + star_speed * 50)
+
+        if stars[i][0] < 0 or stars[i][1] < 0 or stars[i][0] >= 15 or stars[i][1] >= 15:
+          stars[i] = (random.uniform(4, 11), random.uniform(4, 11), 0)
+           
+        v = stars[i][2]
+
+        unicornhathd.set_pixel(stars[i][0], stars[i][1], v, v, v)
+
+      unicornhathd.show()
+
+except KeyboardInterrupt:
+    unicornhathd.off()  
