ec9a30ecd31a1002328b27e7f4f7c533faabcd1c
Nicole Parrot <cleoqc1124@gmail.com>
2017-11-25 14:23:43 -0500

    Add an example that displays current IP on the unicorn hat. Python file and service file to set up to display at boot time

---
+++ examples/show_my_ip.py
@@ -0,0 +1,53 @@
+import socket
+import time
+import unicornhathd
+try:
+    from PIL import Image, ImageDraw, ImageFont
+except ImportError:
+    exit("This script requires the pillow module\nInstall with: sudo pip install pillow")
+
+# get IP address
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+my_ip = s.getsockname()[0]
+print(my_ip)
+s.close()
+
+colours = (255,255,0)
+
+
+font_file = "/usr/share/fonts/truetype/freefont/FreeSansBold.ttf"
+font_size = 12
+
+width, height = unicornhathd.get_shape()  # 16,16 by default
+
+text_x = width
+text_y = 0
+
+font = ImageFont.truetype(font_file, font_size)
+
+text_width, text_height = width, 0
+print(text_width)
+
+w, h = font.getsize(my_ip)
+text_width += w + width  # add some padding so the ip scrolls off the unicorn hat
+text_height = max(text_height,h, 16)  # no more than the size of the unicorn hat
+
+image = Image.new("RGB", (text_width, text_height), (0,0,0))
+draw = ImageDraw.Draw(image)
+draw.text((text_x, text_y), my_ip, colours, font=font)
+
+
+# DISPLAY
+unicornhathd.rotation(0)
+for scroll in range(text_width - width):
+    for x in range(width):
+        for y in range(height):
+            pixel = image.getpixel((x+scroll, y))
+            r, g, b = [int(n) for n in pixel]
+            unicornhathd.set_pixel(width-1-x, y, r, g, b)
+    unicornhathd.show()
+    time.sleep(0.02)
+unicornhathd.off()
+
+
