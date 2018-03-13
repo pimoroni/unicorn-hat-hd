ef8efa05d11632547e0fd19a63671e296c912ccd
Phil Howard <phil@gadgetoid.com>
2018-03-13 13:08:12 +0000

    Tweaked line example

--- examples/line.py
+++ examples/line.py
@@ -9,24 +9,25 @@ while True:
 
         unicornhathd.clear()
         unicornhathd.rotation(rotation)
+        unicornhathd.set_pixel(0, 0, 64, 64, 64)
         unicornhathd.show()
         time.sleep(0.5)
 
-        for x in range(16):
+        for x in range(1,16):
             unicornhathd.set_pixel(x, 0, 255, 0, 0)
             unicornhathd.show()
             time.sleep(0.5 / 16)
 
         time.sleep(0.5)
 
-        for y in range(16):
+        for y in range(1,16):
             unicornhathd.set_pixel(0, y, 0, 0, 255)
             unicornhathd.show()
             time.sleep(0.5 / 16)
 
         time.sleep(0.5)
 
-        for b in range(16):
+        for b in range(1,16):
             unicornhathd.set_pixel(b, b, 0, 255, 0)
             unicornhathd.show()
             time.sleep(0.5 / 16)
