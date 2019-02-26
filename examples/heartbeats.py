c14d1ae9a771daa6d4de8facd24c7ff68a2fb237
Tanya Fish <tanyafxg@gmail.com>
2019-02-26 12:02:03 +0000

    Tidied code, changed imports to full names

--- examples/heartbeats.py
+++ examples/heartbeats.py
@@ -1,11 +1,11 @@
-import unicornhathd as unicorn
+import unicornhathd
 import time
 import colorsys
 import numpy
 
-unicorn.brightness(1)
+unicornhathd.brightness(1)
 # need to rotate the image to have the heart the right way up
-unicorn.rotation(90)
+unicornhathd.rotation(90)
 
 heart = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
@@ -36,10 +36,10 @@ while True:
                 s = 1.0  # saturation at the top of the red scale
                 v = heart[x, y] / float(i)  # brightness depends on range
                 r, g, b = colorsys.hsv_to_rgb(h, s, v)  # convert hsv back to RGB
-                r = int(rgb[0]*255.0)  # makes 0-1 range > 0-255 range
-                g = int(rgb[1]*255.0)
-                b = int(rgb[2]*255.0)
-                unicorn.set_pixel(x, y, r, g, b)  # sets pixels on the hat
-        unicorn.show()  # show the pixels
+                red = int(r*255.0)  # makes 0-1 range > 0-255 range
+                green = int(g*255.0)
+                blue = int(b*255.0)
+                unicornhathd.set_pixel(x, y, red, green, blue)  # sets pixels on the hat
+        unicornhathd.show()  # show the pixels
         time.sleep(0.005)  # tiny gap, sets frames to a smooth 200/sec
     time.sleep(0.8)  # waiting time between heartbeats
