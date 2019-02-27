778bb6f57f61c2db3ba9e94eb3689119c2dd0998
Phil Howard <phil@gadgetoid.com>
2019-02-27 16:29:56 +0000

    Minor linting fixes

--- examples/heartbeats.py
+++ examples/heartbeats.py
@@ -3,7 +3,7 @@ import time
 import colorsys
 import numpy
 
-print ("""Unicorn HAT HD: Heart Beats
+print("""Unicorn HAT HD: Heart Beats
 
 Displaying a beating heart...
 
@@ -41,16 +41,16 @@ heart = numpy.array(heart)
 while True:
     #  go through the range 1-10 backwards, then back up
     #  the 2* makes a ba-BUMP for the heart
-    for i in 2 * (range(10,0,-1) + range(1, 10)):
+    for i in 2 * (range(10, 0, -1) + range(1, 10)):
         for y in range(16):
             for x in range(16):
                 h = 0.0  # red
                 s = 1.0  # saturation at the top of the red scale
                 v = heart[x, y] / float(i)  # brightness depends on range
                 r, g, b = colorsys.hsv_to_rgb(h, s, v)  # convert hsv back to RGB
-                red = int(r*255.0)  # makes 0-1 range > 0-255 range
-                green = int(g*255.0)
-                blue = int(b*255.0)
+                red = int(r * 255.0)  # makes 0-1 range > 0-255 range
+                green = int(g * 255.0)
+                blue = int(b * 255.0)
                 unicornhathd.set_pixel(x, y, red, green, blue)  # sets pixels on the hat
         unicornhathd.show()  # show the pixels
         time.sleep(0.005)  # tiny gap, sets frames to a smooth 200/sec
