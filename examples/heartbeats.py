b05f62955c1177735700bdcc0b5a164ffa4f2409
Robert <robtom_5@mac.com>
2019-04-11 16:32:46 +0100

    Tidied pattern definition and brightness
    Tidied up the definition of the brightness patterns for readabilty
    Inverted the direction of brightness so that a high value in the
    pattern corresponds to a high pixel brightess. This now matches
    the unicornhathd.brightness behaviour

--- examples/heartbeats.py
+++ examples/heartbeats.py
@@ -4,6 +4,7 @@ import unicornhathd
 import time
 import colorsys
 import numpy
+import itertools
 
 print("""Unicorn HAT HD: Heart Beats
 
@@ -18,8 +19,10 @@ Press Ctrl+C to exit!
 """)
 
 unicornhathd.brightness(1)
-# need to rotate the image to have the heart the right way up
-unicornhathd.rotation(90)
+
+# We rotate the heart to be the same orientation as the text on the rear
+# of the Unicorn Hat HD
+unicornhathd.rotation(270)
 
 heart = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
@@ -38,20 +41,28 @@ heart = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
-heartbeat = (range(10, 0, -1), range(1, 5),range(5, 0, -1), range(1, 10))
-
 heart = numpy.array(heart)
+
+# Define the brightness levels for the heartbeat (lower numbers are dimmer)
+# We let the minimum brightness be 1 so that there is still a visible heart
+rising = range(1, 10, 1) # [1...9]
+ba = range(10, 5, -1) # [10...6]
+dum = range(5, 10, 1) # [5...9]
+falling = range(10, 0, -1) # [10...1]
+
+# Join the ranges together
+pattern = (rising, ba, dum, falling)
+brightness_levels = list(itertools.chain.from_iterable(pattern))
 
 try:
     while True:
-        #  go through the range 1-10 backwards, then back up
-        #  the 2* makes a ba-BUMP for the heart
-        for i in [elem for ranges in heartbeat for elem in ranges]:
-            for y in range(16):
-                for x in range(16):
+        # Go through each brightness level in the pattern
+        for level in brightness_levels:
+            for x in range(16):
+                for y in range(16):
                     h = 0.0  # red
                     s = 1.0  # saturation at the top of the red scale
-                    v = heart[x, y] / float(i)  # brightness depends on range
+                    v = heart[x, y] * float(level/10) # brightness depends on range
                     r, g, b = colorsys.hsv_to_rgb(h, s, v)  # convert hsv back to RGB
                     red = int(r * 255.0)  # makes 0-1 range > 0-255 range
                     green = int(g * 255.0)
